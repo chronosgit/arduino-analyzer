bb74aa079465a56209213abf8df4756cd03188cf
chronosgit <warfacemeister@mail.ru>
2024-11-08 18:18:00 +0500

    ✨ feat: added MyHeader layout component

---
+++ components/layout/my-header/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+	import IconColorsWrapper from '~/components/ui/IconColorsWrapper.vue';
+	import { IconBurger } from '~/components/ui/icons';
+
+	const openMySidebar = inject<() => void>('openMySidebar', () => {
+		console.log('Injected openMySidebar is undefined');
+	});
+</script>
+
+<template>
+	<header class="p-2 dark:bg-zinc-900">
+		<ClientOnly>
+			<button @click="openMySidebar">
+				<IconColorsWrapper>
+					<IconBurger class="scale-150" />
+				</IconColorsWrapper>
+			</button>
+		</ClientOnly>
+	</header>
+</template>
+
+<script lang="ts">
+	export default {
+		name: 'LayoutMyHeader',
+	};
+</script>
