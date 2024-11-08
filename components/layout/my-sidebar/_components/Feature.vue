f54022b4e1bed6a584cb1d0f301807139b885a7a
chronosgit <warfacemeister@mail.ru>
2024-11-08 19:39:54 +0500

    🚧 wip: working on MySidebar layout component

---
+++ components/layout/my-sidebar/_components/Feature.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+	import IconColorsWrapper from '~/components/ui/IconColorsWrapper.vue';
+</script>
+
+<template>
+	<div class="cursor-pointer group/feat flex items-center gap-2">
+		<ClientOnly>
+			<IconColorsWrapper
+				class="group-hover/feat:text-indigo-500 dark:group-hover/feat:text-indigo-400"
+			>
+				<slot name="icon" />
+			</IconColorsWrapper>
+		</ClientOnly>
+
+		<p
+			class="transition group-hover/feat:text-indigo-500 dark:group-hover/feat:text-indigo-400 font-medium text-zinc-400"
+		>
+			<slot />
+		</p>
+	</div>
+</template>
