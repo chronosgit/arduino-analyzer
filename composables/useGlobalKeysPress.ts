0235f4e1715903ec30b4d2b232ad3033c839c7da
chronosgit <warfacemeister@mail.ru>
2024-11-08 18:19:49 +0500

    ✨ feat: updated useGlobalKeysPress composable

--- composables/useGlobalKeysPress.ts
+++ composables/useGlobalKeysPress.ts
@@ -5,17 +5,28 @@ export default function () {
 
 		// Toggle color theme
 		if (isCtrl && key === 'q') {
-			const { $toggleMode } = useNuxtApp();
+			const { $isDarkMode, $toggleMode } = useNuxtApp();
+
+			// WARN: for debug purposes
+			console.log('Dark mode value before the change: ', $isDarkMode.value);
 
 			$toggleMode();
 		}
 	};
 
+	const onWindowVisChange = () => {
+		if (document.visibilityState === 'visible') {
+			window.focus();
+		}
+	};
+
 	onMounted(() => {
 		window.addEventListener('keydown', onDocumentKeyPress);
+		document.addEventListener('visibilitychange', onWindowVisChange);
 	});
 
 	onBeforeMount(() => {
 		window.removeEventListener('keydown', onDocumentKeyPress);
+		document.removeEventListener('visibilitychange', onWindowVisChange);
 	});
 }
