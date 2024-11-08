3802a633c815b6640925b8d20571cf6ec75a593e
chronosgit <warfacemeister@mail.ru>
2024-11-08 19:39:27 +0500

    ✨ feat: updated icons

--- components/ui/IconColorsWrapper.vue
+++ components/ui/IconColorsWrapper.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="dark:hover:text-white transition-colors text-zinc-400 hover:text-black dark:text-zinc-400"
+		class="dark:hover:text-indigo-400 flex justify-between items-center transition-colors hover:text-indigo-500 text-zinc-400"
 	>
 		<slot />
 	</div>
--- components/ui/icons/index.ts
+++ components/ui/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as IconLocalization } from './Localization.vue';
 export { default as IconBurger } from './Burger.vue';
 export { default as IconClose } from './Close.vue';
+export { default as IconSunWithClouds } from './SunWithCloud.vue';
+export { default as IconMoonWithCloud } from './MoonWithCloud.vue';
