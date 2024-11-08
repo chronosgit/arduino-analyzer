f6da5c47c6a488abb1fdb66f28835875e31b36ce
chronosgit <warfacemeister@mail.ru>
2024-11-08 18:49:17 +0500

    ✨ feat: updated IconColorsWrapper ui component

--- components/ui/IconColorsWrapper.vue
+++ components/ui/IconColorsWrapper.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="dark:hover:text-white transition-colors text-zinc-500 hover:text-black dark:text-zinc-400"
+		class="dark:hover:text-white transition-colors text-zinc-400 hover:text-black dark:text-zinc-400"
 	>
 		<slot />
 	</div>
