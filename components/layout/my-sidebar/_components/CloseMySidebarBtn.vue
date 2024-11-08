02c20ddd7cf84f3d94390a8100518ac35d48ea40
chronosgit <warfacemeister@mail.ru>
2024-11-08 18:18:24 +0500

    ✨ feat: added MySidebar layout component

---
+++ components/layout/my-sidebar/_components/CloseMySidebarBtn.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+	import { IconClose } from '~/components/ui/icons';
+
+	// Improved function to handle button click
+	const onBtnClick = (attrs: { onClick?: () => void }) => {
+		const fallthroughOnClick = attrs.onClick;
+
+		if (!fallthroughOnClick) return;
+
+		fallthroughOnClick();
+	};
+</script>
+
+<template>
+	<ClientOnly>
+		<button
+			class="flex justify-center items-center text-zinc-500 dark:hover:text-white transition-colors hover:text-black absolute top-0 right-0 -translate-x-1 translate-y-2"
+			@click="onBtnClick($attrs)"
+		>
+			<IconClose class="scale-150" />
+		</button>
+	</ClientOnly>
+</template>
---
+++ components/layout/my-sidebar/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+	import CloseMySidebarBtn from './_components/CloseMySidebarBtn.vue';
+
+	const props = defineProps<{ isActive: boolean }>();
+	const emit = defineEmits<{ (e: 'closeMySidebar'): void }>();
+</script>
+
+<template>
+	<aside
+		class="pt-6 min-w-[30%] fixed left-0 top-0 px-5 transition-transform bottom-0 dark:bg-zinc-950 bg-zinc-300"
+		:class="{
+			'translate-x-0': props.isActive,
+			'-translate-x-full': !props.isActive,
+		}"
+	>
+		<!-- Snapped to top-right corner -->
+		<CloseMySidebarBtn @click="emit('closeMySidebar')" />
+	</aside>
+</template>
+
+<script lang="ts">
+	export default {
+		name: 'LayoutMySidebar',
+	};
+</script>
