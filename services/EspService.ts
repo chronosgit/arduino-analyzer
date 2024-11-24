3afdac739deb9ca711099d2ceebfaabe7abf9885
chronosgit <warfacemeister@mail.ru>
2024-11-24 15:27:26 +0500

    🎨 style: updated server api architecture and deps

--- services/EspService.ts
+++ services/EspService.ts
@@ -3,7 +3,7 @@ import type IServerApiResponse from '~/interfaces/IServerApiResponse';
 // eslint-disable-next-line
 class EspService {
 	static async checkEspState() {
-		return $fetch<IServerApiResponse<boolean | null>>('/api/healthcheck');
+		return $fetch<IServerApiResponse<boolean | null>>('/api/esp/healthcheck');
 	}
 }
 
