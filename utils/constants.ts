29b6e58af99d030ee40802a2fcd9eb32f75989fb
chronosgit <warfacemeister@mail.ru>
2024-11-26 20:38:33 +0500

    ✨ feat: updated the 'constant' utility

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,4 +2,5 @@ export default {
 	moderateGasThreshhold: 600,
 	maxColdTemperatureInCelcius: 0,
 	maxWarmTemperatureInCelcius: 25,
+	gasTypes: ['methane', 'hydrogen', 'smoke', 'butane', 'ethanol', 'co', 'lpg'],
 };
