803618deca7644ba2485ab14d273bc834e37cdb7
chronosgit <warfacemeister@mail.ru>
2024-11-28 18:30:10 +0500

    🚧 wip: working on filters

--- components/shared/MultiSelect.vue
+++ components/shared/MultiSelect.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import SelectBox from '~/components/ui/SelectBox.vue';
+
+	const props = defineProps<{
+		options: { name: string; label: string; isSelected: boolean }[];
+	}>();
+</script>
 
 <template>
-	<p>MockTextHere</p>
+	<div class="">
+		<div v-for="o in props.options" :key="o.name" class="">
+			<p>{{ o.label }}</p>
+
+			<SelectBox :is-selected="o.isSelected" />
+		</div>
+	</div>
 </template>
